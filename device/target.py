7928786ffab5ef791bd9b07f69353b047a714c4a
Jim Knudstrup <jim.knudstrup@gmail.com>
2025-08-15 01:04:32 -0400

    Start working out TargetController class

--- device/target.py
+++ device/target.py
@@ -1,5 +1,25 @@
 import asyncio
+from config import config
 
+"""Target Controller class. Tracks target state and handles interactions with the target hardware components. Interacts with target_server via event bus."""
+
+class TargetController:
+    def __init__(self):
+        self.id = config.get('node_id')
+        self.hit_value = config.get('hit_value')
+        
+    def stand_up(self):
+        # TODO: Send command to servo to stand up target.
+        pass
+    
+    def lay_down(self):
+        # TODO: Send command to servo to lay down target.
+        pass
+    
+    def activate_target(self, duration):
+        """TODO: Stands up target. If the target registers a hit before duration expires, send a POST to the /target_hit endpoint with the target's ID and hit value. Otherwise, sends a message to the same point, but with a hit value of 0."""
+        pass
+    
 def run_target():
     print("Target code coming soon!")
     
